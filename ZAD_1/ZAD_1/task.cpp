0cb56c89f15d23a16314c4b88324818b085ab415
Poiqp <47985019+Poiqp@users.noreply.github.com>
2019-03-06 11:34:40 +0100

    Start

---
+++ ZAD_1/ZAD_1/task.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "task.h"
+
+
+task::task()
+{
+	time_1 = 0;
+	time_2 = 0;
+}
+
+//task::task(z pliku )
+//task::task(randomowo)
+
+task::~task()
+{
+}
---
+++ ZAD_1/ZAD_1/task.h
@@ -0,0 +1,10 @@
+#pragma once
+class task
+{
+public:
+	int time_1;
+	int time_2;
+
+	task();
+	~task();
+};
