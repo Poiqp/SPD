3fb9613fab0c90884f5ebe50f1b46869d8cd8adc
Poiqp-laptop <47985019+Poiqp@users.noreply.github.com>
2019-04-23 22:18:01 +0200

    working update

--- ZAD_4/ZAD_4/ZAD_4.cpp
+++ ZAD_4/ZAD_4/ZAD_4.cpp
@@ -13,7 +13,6 @@
 
 using namespace std;
 
-
 int Cmax(int t, vector<vector<int> > vect) {
 	int sum = 0;
 	sum = vect[0][0];
@@ -30,9 +29,9 @@ bool TestVec(int t, vector<vector<int> > vect) {
 	int sum = 0;
 	sum = vect[0][0];
 
-	for (int i = 0; i < t; i++) {
+	for (int i = 0; i < t-1; i++) {
 		sum += vect[i][1];
-		if (sum < vect[i][0]) {
+		if (sum < vect[i+1][0]) {
 			return false;
 		}
 	}
@@ -43,30 +42,30 @@ int main()
 {
 	// t-tasks, m=3 - rpq 
 	int t, m;
-	ifstream data("in50.txt");
+	ifstream data("in2.txt");
 	data >> t;
 	data >> m;
+	vector<vector<int> > vect(t);
 
 	//filing up vector 
-	vector<vector<int> > vect(t);
-	
 	for (int i = 0; i < t; i++) {
 		vector<int> vectTmp;
 		vect.push_back(vectTmp);
 	}
-
+	// vect[t][R=0,P=1,Q=2]
 	for (int i = 0; i < t; i++) {
 		int tmp;
 		data >> tmp;
 		vect[i].push_back(tmp);
 		data >> tmp;
-		vect[i].push_back(tmp);
+		vect[i].push_back(tmp);	
 		data >> tmp;
 		vect[i].push_back(tmp);
 	}
-	// vect[t][R=0,P=1,Q=2]
 	// cout << vect[1][0] << endl;
+
 
 	cout << TestVec(t,vect)<<endl;
 	cout << Cmax(t, vect) << endl;
-}
+
+} 
