a01b9671e14a38cd23ede304fd1be288afa9307f
Poiqp-laptop <47985019+Poiqp@users.noreply.github.com>
2019-04-23 14:19:28 +0200

    .

---
+++ ZAD_4/ZAD_4/ZAD_4.cpp
@@ -0,0 +1,72 @@
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <random>
+#include <time.h>
+#include <ctime>
+#include <cstdlib>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int Cmax(int t, vector<vector<int> > vect) {
+	int sum = 0;
+	sum = vect[0][0];
+
+	for (int i = 0; i < t; i++) {
+		sum += vect[i][1];
+	}
+	
+	sum += vect[t-1][2];
+	return sum;
+}
+
+bool TestVec(int t, vector<vector<int> > vect) {
+	int sum = 0;
+	sum = vect[0][0];
+
+	for (int i = 0; i < t; i++) {
+		sum += vect[i][1];
+		if (sum < vect[i][0]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	// t-tasks, m=3 - rpq 
+	int t, m;
+	ifstream data("in50.txt");
+	data >> t;
+	data >> m;
+
+	//filing up vector 
+	vector<vector<int> > vect(t);
+	
+	for (int i = 0; i < t; i++) {
+		vector<int> vectTmp;
+		vect.push_back(vectTmp);
+	}
+
+	for (int i = 0; i < t; i++) {
+		int tmp;
+		data >> tmp;
+		vect[i].push_back(tmp);
+		data >> tmp;
+		vect[i].push_back(tmp);
+		data >> tmp;
+		vect[i].push_back(tmp);
+	}
+	// vect[t][R=0,P=1,Q=2]
+	// cout << vect[1][0] << endl;
+
+	cout << TestVec(t,vect)<<endl;
+	cout << Cmax(t, vect) << endl;
+}
