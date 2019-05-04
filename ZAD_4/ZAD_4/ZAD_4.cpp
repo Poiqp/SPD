87447689acb79deb09898bc48f1d48bce81e2667
Poiqp-laptop <47985019+Poiqp@users.noreply.github.com>
2019-05-04 16:30:36 +0200

    Working without loops

--- ZAD_4/ZAD_4/ZAD_4.cpp
+++ ZAD_4/ZAD_4/ZAD_4.cpp
@@ -10,6 +10,21 @@
 
 using namespace std;
 
+//show vector 
+void PrintVect(vector<vector<int> > vect) {
+	for (const vector<int> &v : vect)
+	{
+		for (int x : v) cout << x << ' ';
+		cout << endl;
+	}
+	cout << endl;
+}
+
+//sort by a column | od najwiekszego do najmniejszego 
+bool SortByCol(const vector<int>& v1, const vector<int>& v2) {
+	return v1[2] > v2[2];
+}
+
 //count Cmax of given vector 
 int Cmax(int t, vector<vector<int> > vect) {
 	int sum = 0;
@@ -18,8 +33,8 @@ int Cmax(int t, vector<vector<int> > vect) {
 	for (int i = 0; i < t; i++) {
 		sum += vect[i][1];
 	}
-	
-	sum += vect[t-1][2];
+
+	sum += vect[t - 1][2];
 	return sum;
 }
 
@@ -28,52 +43,45 @@ bool TestVec(int t, vector<vector<int> > vect) {
 	int sum = 0;
 	sum = vect[0][0];
 
-	for (int i = 0; i < t-1; i++) {
+	for (int i = 0; i < t - 1; i++) {
 		sum += vect[i][1];
-		if (sum < vect[i+1][0]) {
+		if (sum < vect[i + 1][0]) {
 			return false;
 		}
 	}
 	return true;
 }
 
-//delete a task with its RPQ 
-void DeleteRow(vector<vector<int> > & vect, int row) {
+//gotowe R wzglêdem czasu vect -> ready
+void GetReady(vector<vector<int> > & vect, vector<vector<int> > & ready, int time) {
+	vector<int> tmp(3, 0);
 
-	if (vect.size() > row)
-	{
-		vect.erase(vect.begin() + row);
-	}
-}
-
-//finding the smallest R and delete corresponding tasks from vector, return smallest R's
-vector<vector<int> > ExtractSmallest(int & t, vector<vector<int> > & vect, int time) {
-	vector<vector<int> > sorted;
-	vector<int> tmp(3,0);
-	vector<bool> rowsToDelete(vect.size(),false);
-
-	for (int i = 0; i < t; i++) {
+	for (int i = 0; i < vect.size(); i++) {
 		if (vect[i][0] == time) {
 			tmp[0] = vect[i][0];
 			tmp[1] = vect[i][1];
 			tmp[2] = vect[i][2];
-			sorted.push_back(tmp);
-			rowsToDelete[i] = true;
-			t--;
+			ready.push_back(tmp);
+			vect.erase(vect.begin() + i);
+			i--;
 		}
 	}
-
-	for (int i = 0; i < vect.size(); i++)
-	{
-		while(rowsToDelete[i] == true) {
-			DeleteRow(vect, i);
-			rowsToDelete.erase(rowsToDelete.begin() + i);
-		}
-	}
-
-	return sorted;
 }
 
+//najwieksze Q , ready -> order
+void GetOrder(vector<vector<int> > & ready, vector<vector<int> > & order) {
+	vector<int> tmp(3, 0);
+
+	sort(ready.begin(), ready.end(), SortByCol);
+
+	while (!ready.empty()) {
+		tmp[0] = ready[0][0];
+		tmp[1] = ready[0][1];
+		tmp[2] = ready[0][2];
+		order.push_back(tmp);
+		ready.erase(ready.begin());
+	}
+}
 
 
 int main()
@@ -97,50 +105,45 @@ int main()
 		data >> tmp;
 		vect[i].push_back(tmp);
 		data >> tmp;
-		vect[i].push_back(tmp);	
+		vect[i].push_back(tmp);
 		data >> tmp;
 		vect[i].push_back(tmp);
 	}
 
-	
+
 	int time = 0;
-	vector<vector<int> > smallest;
-	vector<vector<int> > done;
-	
-	
-	
-	// jak sprawdzic czy jest pusty skoro size nie dziala 
-	while(!vect.empty()) {
+	vector<vector<int> > ready;
+	vector<vector<int> > order;
 
-		cout << "____________time: " << time << "_______________size: " << vect.size() << endl;
-		
-		for (const std::vector<int> &v : vect)
-		{
-			for (int x : v) std::cout << x << ' ';
-			std::cout << std::endl;
-		}
+	time = 1;
+	cout << "__________CZAS= " << time << endl;
+	PrintVect(vect);
+	GetReady(vect, ready, time);
+	GetOrder(ready, order);
+	PrintVect(order);
 
-		cout << endl;
-		
-		smallest = ExtractSmallest(t, vect, time);
-		
-		for (const std::vector<int> &k : vect)
-		{
-			for (int z : k) std::cout << z << ' ';
-			std::cout << std::endl;
-		}
-		
-		cout << endl;
-		cout << "sorted   : " << endl;
-		cout << endl;
-		for (const std::vector<int> &m : smallest)
-		{
-			for (int o : m) std::cout << o << ' ';
-			std::cout << std::endl;
-		}
-	
-		time++;
-	}
-	
-	
-} 
+	time = 2;
+	cout << "__________CZAS= " << time << endl;
+	PrintVect(vect);
+	GetReady(vect, ready, time);
+	GetOrder(ready, order);
+	PrintVect(order);
+
+	time = 3;
+	cout << "__________CZAS= " << time << endl;
+	PrintVect(vect);
+	GetReady(vect, ready, time);
+	GetOrder(ready, order);
+	PrintVect(order);
+
+	time = 4;
+	cout << "__________CZAS= " << time << endl;
+	PrintVect(vect);
+	GetReady(vect, ready, time);
+	GetOrder(ready, order);
+	PrintVect(order);
+
+}
+
+
+
