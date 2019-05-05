1232ad86ed841e5b2d55c60a39cc6dd1b009f1b2
Poiqp-laptop <47985019+Poiqp@users.noreply.github.com>
2019-05-05 11:19:07 +0200

    loop changes
    GetOrder changed
    final loop changed
    
    Cmax needs a change

--- ZAD_4/ZAD_4/ZAD_4.cpp
+++ ZAD_4/ZAD_4/ZAD_4.cpp
@@ -25,16 +25,16 @@ bool SortByCol(const vector<int>& v1, const vector<int>& v2) {
 	return v1[2] > v2[2];
 }
 
-//count Cmax of given vector 
-int Cmax(int t, vector<vector<int> > vect) {
+//count Cmax of given vector ALE NIE DO KONCA BO NIE DOLICZA DZIUR 
+int Cmax(vector<vector<int> > vect) {
 	int sum = 0;
 	sum = vect[0][0];
 
-	for (int i = 0; i < t; i++) {
+	for (int i = 0; i < vect.size(); i++) {
 		sum += vect[i][1];
 	}
 
-	sum += vect[t - 1][2];
+	sum += vect[vect.size() - 1][2];
 	return sum;
 }
 
@@ -57,7 +57,7 @@ void GetReady(vector<vector<int> > & vect, vector<vector<int> > & ready, int tim
 	vector<int> tmp(3, 0);
 
 	for (int i = 0; i < vect.size(); i++) {
-		if (vect[i][0] == time) {
+		if (vect[i][0] <= time) {
 			tmp[0] = vect[i][0];
 			tmp[1] = vect[i][1];
 			tmp[2] = vect[i][2];
@@ -69,18 +69,29 @@ void GetReady(vector<vector<int> > & vect, vector<vector<int> > & ready, int tim
 }
 
 //najwieksze Q , ready -> order
-void GetOrder(vector<vector<int> > & ready, vector<vector<int> > & order) {
+int GetOrder(vector<vector<int> > & ready, vector<vector<int> > & order) {
 	vector<int> tmp(3, 0);
 
 	sort(ready.begin(), ready.end(), SortByCol);
 
-	while (!ready.empty()) {
+	//while (!ready.empty()) {
 		tmp[0] = ready[0][0];
 		tmp[1] = ready[0][1];
 		tmp[2] = ready[0][2];
 		order.push_back(tmp);
 		ready.erase(ready.begin());
+	//}
+		return tmp[1];
+}
+
+//najmniejsze R 
+int FindSmallestRTime(vector<vector<int> >  vect) {
+	int smallestR = vect[0][0];
+
+	for (int i = 0; i < vect.size(); i++) {
+		if (vect[i][0] < smallestR) { smallestR = vect[i][0]; }
 	}
+	return smallestR;
 }
 
 
@@ -88,7 +99,7 @@ int main()
 {
 	// t-tasks, m=3 - rpq 
 	int t, m;
-	ifstream data("in2.txt");
+	ifstream data("in50.txt");
 	data >> t;
 	data >> m;
 	vector<vector<int> > vect;
@@ -112,9 +123,36 @@ int main()
 
 
 	int time = 0;
+	int getP = 0;
 	vector<vector<int> > ready;
 	vector<vector<int> > order;
 
+	time = FindSmallestRTime(vect);
+
+	//POPRZEDNIE WYKONANIE PRZERZUCALO Q CALOSCIOWO | SPROBOWAC PO KOLEI ZGODNIE Z PSEUDOKODEM 
+	while (vect.empty() != 1 || ready.empty() != 1) {
+
+		GetReady(vect, ready, time);
+
+		if (ready.empty() == 1 ) {
+			time = FindSmallestRTime(vect);
+		}
+		else {
+			getP = GetOrder(ready, order);
+			time += getP;
+		}
+	}
+
+
+	PrintVect(order);
+
+	cout << TestVec(t, order) << endl;
+	cout << time << endl;
+	cout << order.size() << endl;
+//	cout << Cmax(order) << endl;  NAPISAC JESZCZE RAZ CMAXA Z UWZGLEDNIENIEM DZIUR !!!
+}
+
+/*
 	time = 1;
 	cout << "__________CZAS= " << time << endl;
 	PrintVect(vect);
@@ -142,8 +180,6 @@ int main()
 	GetReady(vect, ready, time);
 	GetOrder(ready, order);
 	PrintVect(order);
-
-}
-
+	*/
 
 
